a9c3df067de5235e964ddd5166a21e15997056f8
mishell778 <mishell.r.123@gmail.com>
2025-08-08 23:23:12 -0400

    feat: create project setup

---
+++ portfolio/src/App.js
@@ -0,0 +1,37 @@
+import logo from './logo.svg';
+import './App.css';
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}*/
+
+function App() {
+  return (
+    <div className="text-center mt-10">
+      <h1 className="text-4xl font-bold underline text-blue-600">
+        Hello, Tailwind CSS!
+      </h1>
+      <p className="mt-4 text-gray-700">Your Tailwind setup is working.</p>
+    </div>
+  );
+}
+
+export default App;
