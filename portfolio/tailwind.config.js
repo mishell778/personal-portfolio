02d5a925ee5b1d7e57d5b684fa941b10a3cc143c
mishell778 <mishell.r.123@gmail.com>
2025-08-15 11:50:00 -0400

    feat add features for main page

--- portfolio/tailwind.config.js
+++ portfolio/tailwind.config.js
@@ -4,7 +4,33 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        display: ['"Playfair Display"', 'serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        montserrat: ['Montserrat', 'sans-serif'],
+        quicksand: ['Quicksand', 'sans-serif'],
+        bodoni: ['Bodoni Moda', 'sans-serif'],
+        abel: ['Abel', 'sans-serif'],
+        philosopher: ['Philosopher', 'sans-serif'], 
+      },
+      backgroundImage: {
+        'animated-gradient': 'linear-gradient(270deg, #ede7e7ff, #deccd8ff,  #bcb7bcff)', 
+        // 'animated-gradient': 'linear-gradient(270deg, #fed6f1ff, #ddd8f5ff, #f2ead3ff)'
+      },
+      backgroundSize: {
+        '400%': '400% 400%',
+      },
+      animation: {
+        'gradientShift': 'gradientShift 10s ease infinite',
+      },
+      keyframes: {
+        gradientShift: {
+          '0%, 100%': { 'background-position': '0% 50%' },
+          '50%': { 'background-position': '100% 50%' },
+        },
+      },
+    },
   },
   plugins: [],
 }
